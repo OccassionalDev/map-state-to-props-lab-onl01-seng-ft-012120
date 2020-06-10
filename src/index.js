482a1b9a07b6c33b34c30ac53e577afc277cd11f
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-10 21:41:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux' 
 import { Provider } from 'react-redux'
+import manageUsers from './reducers/manageUsers'
 import App from './App'
 
-import manageUsers from './reducers/manageUsers'
+
 
 
 // add imports and code
