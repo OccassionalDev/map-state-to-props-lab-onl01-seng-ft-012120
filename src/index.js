5343cb37df65a783c66771aa658448c7c53f27ed
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-10 21:43:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import { Provider } from 'react-redux'
 import manageUsers from './reducers/manageUsers'
 import App from './App'
 
-
-
-
-// add imports and code
 const store = createStore(manageUsers)
-
 
 ReactDOM.render(
   <Provider store={ store }>
